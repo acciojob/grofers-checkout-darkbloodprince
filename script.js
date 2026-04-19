06079eef8b6a1f97a39b93635e53790205dc09d2
Dasari Vamsi <44261806+darkbloodprince@users.noreply.github.com>
2026-04-19 14:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+
+  let total = 0;
+
+  prices.forEach((p) => {
+    total += Number(p.textContent);
+  });
+
+  // Create new row
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+
+  td.setAttribute("colspan", "2");
+  td.textContent = total;
+
+  tr.appendChild(td);
+
+  document.querySelector("table").appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
